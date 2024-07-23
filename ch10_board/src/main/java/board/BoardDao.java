d6ce6ea691d68a9855ef1d49e1be9b4b14d3f506
limhyeonseoung <coldfela99@naver.com>
2024-07-23 18:16:07 +0900

    2024-07-23

--- ch10_board/src/main/java/board/BoardDao.java
+++ ch10_board/src/main/java/board/BoardDao.java
@@ -1,4 +1,4 @@
-package board; 
+package board;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -15,29 +15,122 @@ public class BoardDao {
 		pool = DBConnectionMgr.getInstance();
 	}
 	
-	
 	// 게시판 리스트
-	public ArrayList<Board> getBoardList() {
+	public ArrayList<Board> getBoardList(String keyField,String keyWord, int start, int end) {
 		ArrayList<Board> alist = new ArrayList<Board>();
+		
 		try {
 			con = pool.getConnection();
-			sql = "select * from Board";	
-			rs = con.createStatement().executeQuery(sql);
+			if(keyWord == null || keyWord.equals("")) {
+				sql = "select * "
+					+ "  from (select ROWNUM AS RNUM, BT1.* "
+					+ "         from (select * from board order by ref desc, pos) BT1"
+					+ "        )"
+					+ "  where RNUM between ? and ?";
+				pstmt= con.prepareStatement(sql);
+				pstmt.setInt(1, start);
+				pstmt.setInt(2, end);
+			} else {
+				sql = "select * "
+					+ "  from (select ROWNUM AS RNUM, BT1.* "
+					+ "         from (select * from board order by ref desc, pos) BT1"
+					+ "			where " + keyField + " like ?"
+					+ "        )"
+					+ "  where RNUM between ? and ?";
+				pstmt= con.prepareStatement(sql);
+				pstmt.setString(1, "%" + keyWord + "%");
+				pstmt.setInt(2, start);
+				pstmt.setInt(3, end);
+			}
+			
+			rs = pstmt.executeQuery();
 			while(rs.next()) {
 				Board board = new Board();
 				board.setNum(rs.getInt("num"));
 				board.setSubject(rs.getString("subject"));
-				board.setName(rs.getString("name"));			
-				board.setCount(rs.getInt("count"));
-				board.setPos(rs.getInt("pos"));
-				board.setRef(rs.getInt("ref"));
-				board.setDepth(rs.getInt("depth"));
+				board.setName(rs.getString("name"));
 				board.setRegdate(rs.getString("regdate"));
+				board.setCount(rs.getInt("count"));
+				board.setRef(rs.getInt("ref"));
+				board.setPos(rs.getInt("pos"));
+				board.setDepth(rs.getInt("depth"));
 				alist.add(board);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con);
 		}
 		return alist;
 	}
+	
+	// 게시물 총 레코드수
+	public int getTotalCount(String keyField, String keyWord) {
+		int totalCount = 0;
+		
+		try {
+			con = pool.getConnection();
+			if(keyWord == null || keyWord.equals("")) {
+				sql = "select count(num) from board";
+				pstmt = con.prepareStatement(sql);
+			} else {
+				sql = "select count(num) from board where " + keyField + " like ?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, "%" + keyWord + "%");
+			}
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next())
+				totalCount = rs.getInt(1);
+			
+			System.out.println("totalCount : " + totalCount);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con);
+		}
+		return totalCount;
+	}
+	
+	
+	// 조회수 증가
+	public void upCount(int num) {		
+		try {
+			con = pool.getConnection();
+			sql = "update board set count = count+1 where num=" + num;
+			con.createStatement().execute(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con);
+		}
+	}
+	
+	// 게시물 1행 가져오기
+		public Board getOneBoard(int num){		
+			Board board = new Board();
+			
+			try {
+				con = pool.getConnection();
+				sql = "select * from board where num=" + num;
+				rs = con.createStatement().executeQuery(sql);
+				if(rs.next()) {
+					board.setNum(rs.getInt("num"));
+					board.setSubject(rs.getString("subject"));
+					board.setName(rs.getString("name"));
+					board.setRegdate(rs.getString("regdate"));
+					board.setCount(rs.getInt("count"));
+					board.setRef(rs.getInt("ref"));
+					board.setPos(rs.getInt("pos"));
+					board.setDepth(rs.getInt("depth"));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pool.freeConnection(con);
+			}
+			return board;
+		}
 }
